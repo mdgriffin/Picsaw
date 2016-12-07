68c6edfb09eff329ef7222b5472ac33051bac679
Michael Griffin <t00119683@365s.ittralee.ie>
2016-12-07 14:23:05 +0000

    Added Javadoc comments

--- src/SerializedImageSlice.java
+++ src/SerializedImageSlice.java
@@ -15,6 +15,16 @@ public class SerializedImageSlice implements Serializable {
     private int currentXPos;
     private int currentYPos;
 
+    /**
+     * Defined a Serializable version of an Image Slice
+     *
+     * @param imagePiece    The Image Icon that will be inside the Image Slice's JLabel
+     * @param xPos          The current x co-ordinate of the Image Slice
+     * @param yPos          The current y co-ordinate of the Image Slice
+     * @param currentXPos   The correct x co-ordinate of the Image Slice
+     * @param currentYPos   The correct y co-ordinate of the Image Slice
+     */
+
     public SerializedImageSlice(ImageIcon imagePiece, int xPos, int yPos, int currentXPos, int currentYPos) {
         this.imagePiece = imagePiece;
         this.xPos = xPos;
@@ -23,21 +33,51 @@ public class SerializedImageSlice implements Serializable {
         this.currentYPos = currentYPos;
     }
 
+    /**
+     * Getter to retrieve the Image Piece
+     *
+     * @return The Serialized Image Slice's ImageIcon
+     */
+
     public ImageIcon getImagePiece () {
         return imagePiece;
     }
+
+    /**
+     * Getter to retrieve the current x position of the Image Piece
+     *
+     * @return An int representing the current x position of the Image Piece
+     */
 
     public int getXPos () {
         return xPos;
     }
 
+    /**
+     * Getter to retrieve the current y position of the Image Piece
+     *
+     * @return An int representing the current y position of the Image Piece
+     */
+
     public int getYPos () {
         return yPos;
     }
 
+    /**
+     * Getter to retrieve the correct x position of the Image Piece
+     *
+     * @return An int representing the current y position of the Image Piece
+     */
+
     public int getCurrentXPos () {
         return currentXPos;
     }
+
+    /**
+     * Getter to retrieve the correct y position of the Image Piece
+     *
+     * @return An int representing the current y position of the Image Piece
+     */
 
     public int getCurrentYPos () {
         return currentYPos;
